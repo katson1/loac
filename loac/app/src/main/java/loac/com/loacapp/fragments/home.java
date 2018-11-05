2bf287b53b408fa071442c9b46f5cc0e663cc527
Katson <katson.matheus.1@gmail.com>
2018-11-05 08:03:50 -0300

    android

---
+++ loac/app/src/main/java/loac/com/loacapp/fragments/home.java
@@ -0,0 +1,22 @@
+package loac.com.loacapp.fragments;
+
+import android.support.annotation.Nullable;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import loac.com.loacapp.R;
+
+public class home extends Fragment{
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.fragment_home, container, false);
+        return myView;
+    }
+}
